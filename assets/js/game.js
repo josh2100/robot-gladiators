f50799afd2b55aa9b81044d41b510ff82ce8eb96
joshgit <jschermann91@gmail.com>
2021-10-18 08:51:23 -0500

    getPlayerName() added

--- assets/js/game.js
+++ assets/js/game.js
@@ -114,10 +114,6 @@ var startGame = function () {
     //Reset enemy.health before starting a new fight
     pickedEnemyObj.health = randomNumber(40, 60);
 
-    //Use debugger to pause script from running and check what's going on at that
-    //moment in the code
-    //debugger;
-
     //Pass the pickedenemy.name variable's value into the fight function, where it
     //will assume the value of the enemy.name parameter
     fight(pickedEnemyObj);
@@ -192,8 +188,19 @@ var shop = function () {
   }
 };
 
+var getPlayerName = function () {
+  var name = "";
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
